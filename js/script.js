aafca19cc25e2d218771b441406abc6aba20ea33
larissa-amaral <larissa.pereira.amaral@hotmail.com>
2021-07-24 21:45:35 -0300

    Add formulário ao localstorage

--- js/script.js
+++ js/script.js
@@ -4,9 +4,17 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let endereço = document.getElementById('endereço').value;
+    let produtos = document.getElementById('produtos').value;
+    let quantidade = document.getElementById('quantidade').value;
     let data = {
         nome,
         email,
+        telefone,
+        endereço,
+        produtos,
+        quantidade
     }
     let coverData = JSON.stringify(data)
 
